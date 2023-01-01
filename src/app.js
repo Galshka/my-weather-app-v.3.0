7031a99750e8666c4b27387a29614a0ccf71ec56
Galshka <66124329+Galshka@users.noreply.github.com>
2023-01-01 21:14:27 +0200

    change part1
    - created created a function for obtaining weather data
    - created function setting city for a displaying weather

--- src/app.js
+++ src/app.js
@@ -8,14 +8,6 @@ let days = [
   "Saturday",
 ];
 
-let apiKey = "05bfb21a258cdae24d749dd944debfc2";
-let cityStart = "Odesa";
-let h1 = document.querySelector("h1");
-let h2 = document.querySelector("h2");
-let todayTemp = document.querySelector(".today-temp");
-let todayWeatherIcon = document.querySelector("#today-weather-icon");
-let todayHumidity = document.querySelector("#humidity");
-let todayWind = document.querySelector("#wind");
 let forecastDay = document.querySelectorAll(".forecast-days");
 let forecastTemp = document.querySelectorAll(".forecast-temp");
 
@@ -36,28 +28,30 @@ function getCurrentDate() {
   return { dayWeek, time };
 }
 
-// function changeData(event) {
-//   let todayTime = getCurrentDate();
-//   console.log(todayTime.dayWeek);
-//   let dayNumber = days.indexOf(todayTime.dayWeek);
-//   console.log(dayNumber);
-//   forecastDay[0].innerHTML = todayTime.dayWeek;
-// }
-
 function showWeather(response) {
-  console.log(response);
   let temperature = Math.round(response.data.main.temp);
   let humidity = response.data.main.humidity;
-
   let wind = response.data.wind.speed;
+  wind = wind.toFixed(1);
   let weatherDescription = response.data.weather[0].description;
+  weatherDescription =
+    weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
   console.log(weatherDescription);
   let weatherIcon = response.data.weather[0].icon;
   let city = response.data.name;
   let date = getCurrentDate();
 
+  let h1 = document.querySelector("h1");
+  let h2 = document.querySelector("h2");
+  let todayTemp = document.querySelector(".today-temp");
+  let todayWeatherIcon = document.querySelector("#today-weather-icon");
+  let todayWeatherDescription = document.querySelector("#weather-description");
+  let todayHumidity = document.querySelector("#humidity");
+  let todayWind = document.querySelector("#wind");
+
   h1.innerHTML = city;
   h2.innerHTML = `${date.dayWeek}, ${date.time}`;
+  todayWeatherDescription.innerHTML = weatherDescription;
   todayTemp.innerHTML = temperature;
   todayWeatherIcon.setAttribute("src", `icons/${weatherIcon}-100.png`);
   todayWeatherIcon.setAttribute("alt", `${weatherDescription}`);
@@ -65,10 +59,18 @@ function showWeather(response) {
   todayWind.innerHTML = `&nbsp;&nbsp${wind} m/s`;
 }
 
-function showForecast(response) {}
+function search(city) {
+  let apiKey = "05bfb21a258cdae24d749dd944debfc2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityStart}&units=metric&appid=${apiKey}`;
-axios.get(apiUrl).then(showWeather);
+search("Odesa");
+
+function setCity(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#search-form");
+}
 
 let searchButton = document.querySelector("#search-button");
-// searchButton.addEventListener("click", changeData);
+searchButton.addEventListener("submit", setCity);
